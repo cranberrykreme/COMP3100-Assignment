d143e0ca96066af91dba747b163a93506269916d
chrispurkiss <chris.purkiss99@Gmail.com>
2020-05-15 21:41:35 +1000

    cleaning up First_fit class

--- src/First_fit.java
+++ src/First_fit.java
@@ -1,20 +1,19 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+/**
+ * 
+ * @author chrispurkiss
+ * 
+ * First fit is an algorithm to send
+ * a job to the first available or capable server
+ * with the capacity to run that job
+ */
 
 public class First_fit {
 
@@ -33,7 +32,6 @@ public class First_fit {
 	private static final String RESC = "RESC Avail";
 	private static final String RESCCapable = "RESC Capable";
 	private static final String OK = "OK";
-	private static final String ERR2 = "ERR: invalid command (OK)";
 	
 	public First_fit(String address, int port) {
 		try {
@@ -41,34 +39,20 @@ public class First_fit {
 			socket = new Socket(address,port);
 			System.out.println("Connected");
 			
-			outToServer = socket.getOutputStream();
-			out = new DataOutputStream(outToServer);
-			
-			inFromServer = socket.getInputStream();
-			in = new DataInputStream(inFromServer);
-			
 			//new message to Server
-			writeMSG(socket, HELO, out);
+			writeMSG(socket, HELO);
 			
 			//receive message from Server
-			readMSG(socket, in);
+			readMSG(socket);
 			
 			//second message to server
-			writeMSG(socket, AUTH, out);
-			
-			//parse system.xml
-			//File file = new File("/Users/garyguan/Downloads/ds-sim/system.xml");
-			File file = new File("/home/comp335/ds-sim/system.xml");
-			//File file = new File("system.xml");
-			//File file = new File("/Users/chrispurkiss/ds-sim/system.xml");
-			String ans = parse(file);
-			System.out.println(ans);//not needed
+			writeMSG(socket, AUTH);
 			
 			//second message from server
-			readMSG(socket, in);
+			readMSG(socket);
 			
 			//third message to server
-			writeMSG(socket,REDY, out);
+			writeMSG(socket,REDY);
 			
 			//third message from server
 			//readMSG(socket);
@@ -77,10 +61,9 @@ public class First_fit {
 			 * method for going through the servers
 			 * and picking the right server for each job
 			 */
-			int i = 0;
 			while(true) {
 				//reading job from server
-				String error = readMSG(socket, in);
+				String error = readMSG(socket);
 				if(error.contains(NONE) || error.contains(ERR)) {
 					break;
 				}
@@ -100,12 +83,12 @@ public class First_fit {
 				
 				//sending RESC command
 				String job = error.substring(index);
-				writeMSG(socket, RESC + job, out);
+				writeMSG(socket, RESC + job);
 			
-				String servers = readMSG(socket, in);//sends back DATA
+				String servers = readMSG(socket);//sends back DATA
 				
-				writeMSG(socket,OK, out);//sends ok
-				servers = readMSG(socket, in);//first server info
+				writeMSG(socket,OK);//sends ok
+				servers = readMSG(socket);//first server info
 				String foundServer = null;
 				//writing OK while receiving info on servers,
 				//also checks if all info has been sent
@@ -118,29 +101,25 @@ public class First_fit {
 						foundServer = ff(servers, error);
 					}
 
-					writeMSG(socket,OK, out);
-					servers = readMSG(socket, in); //going through the servers available
+					writeMSG(socket,OK);
+					servers = readMSG(socket); //going through the servers available
 					
 				}
 				String jobN = getNumb(error, 2);
-				//job message to server
-//				if(foundServer == null) {
-//					writeMSG(socket,"SCHD " + jobN + " " + ans + " 0", out);
 				 if(foundServer != null) {
 					String servernum = getNumb(foundServer,1);
 					foundServer = getNumb(foundServer,0);
-					writeMSG(socket,"SCHD " + jobN + " " + foundServer + " " +servernum,out);
+					writeMSG(socket,"SCHD " + jobN + " " + foundServer + " " +servernum);
 				}
 				 else {
-					 writeMSG(socket, RESCCapable + job, out);
+					 writeMSG(socket, RESCCapable + job);
 						
-						String serversCap = readMSG(socket, in);//sends back DATA
+						String serversCap = readMSG(socket);//sends back DATA
 						
-						writeMSG(socket,OK, out);//sends ok
-						serversCap = readMSG(socket, in);//first server info
+						writeMSG(socket,OK);//sends ok
+						serversCap = readMSG(socket);//first server info
 						
 						while(!serversCap.substring(0, 1).contains(".")) {
-							String isAvail = getNumb(serversCap, 3);
 							
 							if(foundServer == null) {
 								if(Integer.parseInt(getNumb(serversCap,2)) == 3 || Integer.parseInt(getNumb(serversCap,2)) ==2 ) {
@@ -148,32 +127,31 @@ public class First_fit {
 								}
 							}
 
-							writeMSG(socket,OK, out);
-							serversCap = readMSG(socket, in); //going through the servers available
+							writeMSG(socket,OK);
+							serversCap = readMSG(socket); //going through the servers available
 							
 						}
 						
 						String servernum = getNumb(foundServer,1);
 						foundServer = getNumb(foundServer,0);
-						writeMSG(socket,"SCHD " + jobN + " " + foundServer + " " +servernum,out);
+						writeMSG(socket,"SCHD " + jobN + " " + foundServer + " " +servernum);
 						
 				 }
 				
 				
 				//get response
-				String response = readMSG(socket, in);
+				String response = readMSG(socket);
 				if(response.contains(NONE) || response.contains(ERR)) {
 					break;
 				}
 				
 				//send REDY
-				writeMSG(socket, REDY, out);
-				i++;
+				writeMSG(socket, REDY);
 			}
 			
 			//LAST STAGE: QUIT
-			writeMSG(socket, QUIT, out);
-			readMSG(socket, in);
+			writeMSG(socket, QUIT);
+			readMSG(socket);
 			
 		}  catch (UnknownHostException u) {
 			System.out.println(u);
@@ -193,16 +171,6 @@ public class First_fit {
 		}
 	}//end of main class
 	
-	public static Integer activeServer(String address, String job, int spaces) {
-		String servercore = getNumb(address, spaces);
-		String jobcore = getNumb(job,spaces);
-		
-		int as =0;
-		as = Integer.parseInt(servercore) - Integer.parseInt(jobcore);
-		
-		return as;
-	}
-	
 	/*
 	 * Get the strings to send
 	 * find location to send 
@@ -210,7 +178,10 @@ public class First_fit {
 	 * and send them to client
 	 * 
 	 */
-	private void writeMSG(Socket socket, String msg, DataOutputStream out) throws IOException {
+	private void writeMSG(Socket socket, String msg) throws IOException {
+		outToServer = socket.getOutputStream();
+		out = new DataOutputStream(outToServer);
+		
 		out.write(msg.getBytes());
 		System.out.println("messge sent to server: " + msg);
 		out.flush();
@@ -220,8 +191,9 @@ public class First_fit {
 	 * print out that message has been received
 	 * use message in Client method
 	 */
-	private String readMSG(Socket socket, DataInputStream in) throws IOException {
-		
+	private String readMSG(Socket socket) throws IOException {
+		inFromServer = socket.getInputStream();
+		in = new DataInputStream(inFromServer);
 		
 		byte[] rMSG = new byte[1024];
 		in.read(rMSG);
@@ -231,57 +203,6 @@ public class First_fit {
 		return str;
 		
 	}
-	
-	/*
-	 * get information out of file and return
-	 * the largest server (the one with the most cores)
-	 */
-	private String parse(File file) {
-		try {
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(file);
-			ArrayList<String> str = new ArrayList<String>();
-			ArrayList<Integer> list = new ArrayList<Integer>();
-			
-			doc.getDocumentElement().normalize();
-			
-			NodeList nList = doc.getElementsByTagName("server");
-			
-			if(doc.hasChildNodes()) {
-				for(int i = 0; i < nList.getLength(); i++) {
-					Node node = nList.item(i);
-					
-					if(node.getNodeType() == Node.ELEMENT_NODE) {
-						Element element = (Element) node;
-						
-						str.add(element.getAttribute("type"));
-						list.add(Integer.parseInt(element.getAttribute("coreCount")));
-					}
-				}
-			}
-			
-			int largest = 0;
-			
-			for(int i = 1; i<str.size(); i++) {
-				if(list.get(i) > list.get(largest)) {
-					largest  = i;
-				}
-			}
-			
-			String ans = new String();
-			ans =  str.get(largest);
-			
-			return ans;
-			
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		
-		return "Did not work";
-	}
-	
-	
 	/**
 	 * finds if a server can hold a certain job
 	 * if not returns null
@@ -294,18 +215,18 @@ public class First_fit {
 		
 		String jobMem = null;
 		String jobDisk = null;
+
+		memory = getNumb(address,5);//gets memory for server
+		diskspace = getNumb(address,6);//gets diskspace for server
 		
-	
+		jobMem = getNumb(job,5);//gets memory for job
+		jobDisk = getNumb(job,6);//gets diskspace for job
 		
-		memory = getNumb(address,5);
-		diskspace = getNumb(address,6);
-		
-		jobMem = getNumb(job,5);
-		jobDisk = getNumb(job,6);
-		
-		System.out.println("address memory = " + memory + " job memory = " + jobMem);
-		System.out.println("address diskspace = " + diskspace + " job diskspace = " + jobDisk);
-		
+		/**
+		 * if the memory and diskspace of the server is large enough
+		 * to hold the job then set the server to that server
+		 * if not, then leave it as null
+		 */
 		if(Double.parseDouble(memory) > Double.parseDouble(jobMem) && Double.parseDouble(diskspace) > Double.parseDouble(jobDisk)) {
 			hold = address;
 		}
@@ -325,12 +246,11 @@ public class First_fit {
 		int subindex = 0;
 		String numb = null;
 		
-		if(address.length() < 10) {
-			System.out.println("address is too short at: " + address.length());
+		if(address.length() < 10) {//just in case a shorter message (such as DATA gets through)
 			return null;
 		}
 		
-		for(int temp = 0; temp < address.length(); temp++) {
+		for(int temp = 0; temp < address.length(); temp++) {//gets the index of the required space
 			if(address.charAt(temp) == ' ') {
 				spc++;
 			}
@@ -339,28 +259,21 @@ public class First_fit {
 				break;
 			}
 		}
-		System.out.println(spc + " subindex is: " + subindex);
-		
 		
 		int finalIndex = subindex +1;
-		if(spaces <= 5) {
+		if(spaces <= 5) {//if the space is not for the final number in the message
 			while(address.charAt(finalIndex) != ' ') {
 				finalIndex++;
 			}
-		} else {
+		} else {//if it is
 			finalIndex = address.length();
 		}
 		
-		
-		System.out.println("finalindex is: " + finalIndex);
-		if(spaces != 0) {
+		if(spaces != 0) {//if the space is for the name of the server/job
 			numb = address.substring(subindex+1,finalIndex);
-		} else {
+		} else {//if it is not
 			numb = address.substring(subindex,finalIndex);
 		}
-		
-		
-		System.out.println("string returned " + numb);
 		
 		return numb;
 	}
